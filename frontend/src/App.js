5521829931e7cc2aff662177fb2b6249af46546b
MartyB <martbarc@gmail.com>
2022-06-28 00:30:31 -0400

    Changed card to a class component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
+import React, { useEffect, useState, useRef } from 'react';
+import ReactDOM from 'react-dom/client'
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios'
 import {
   Dialog,
@@ -10,30 +11,38 @@ import {
   Button,
   DialogTitle
 } from "@mui/material";
-import Card from "./Card";
+import Card from "./Card.js";
 import "./App.scss";
 
 
 const uniqueElementsArray = [
   {
-    type: "stock",
-    image: require(`./images/stock.jpg`)
+    type: "stock1",
+    image: require(`./images/player612.jpeg`)
   },
   {
     type: "stock2",
-    image: require(`./images/stock.jpg`)
+    image: require(`./images/player612.jpeg`)
   },
   {
     type: "stock3",
-    image: require(`./images/stock.jpg`)
+    image: require(`./images/player612.jpeg`)
   },
   {
     type: "stock4",
-    image: require(`./images/stock.jpg`)
+    image: require(`./images/player612.jpeg`)
   },
   {
     type: "stock5",
-    image: require(`./images/stock.jpg`)
+    image: require(`./images/player612.jpeg`)
+  },
+  {
+    type: "stock6",
+    image: require(`./images/player612.jpeg`)
+  },
+  {
+    type: "stock7",
+    image: require(`./images/player612.jpeg`)
   },
 ];
 
@@ -145,48 +154,128 @@ export default function App() {
     setCards(shuffleCards(uniqueElementsArray.concat(uniqueElementsArray)));
   };
 
+  const handleAddToBench = () => {
+
+  };
+
+  //const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+return (
+  <Card
+    key={index}
+    card={card}
+    index={index}
+    isDisabled={shouldDisableAllCards}
+    isInactive={checkIsInactive(card)}
+    isFlipped={checkIsFlipped(index)}
+    onClick={handleCardClick}
+  />
+);
+
+return (
+  root.render(
+    <Card
+      key={index}
+      card={card}
+      index={index}
+      isDisabled={shouldDisableAllCards}
+      isInactive={checkIsInactive(card)}
+      isFlipped={checkIsFlipped(index)}
+      onClick={handleCardClick}
+    />
+  )
+);
+*/
+
   return (
     <div className="App">
+
       <header className="App-header">
         <h3>Superclub Manager Mode</h3>
         <div>
           Select cards to move them from bench or starting lineup.
         </div>
       </header>
-      <div className="container">
-        {cards.map((card, index) => {
-          return (
-            <Card
-              key={index}
-              card={card}
-              index={index}
-              isDisabled={shouldDisableAllCards}
-              isInactive={checkIsInactive(card)}
-              isFlipped={checkIsFlipped(index)}
-              onClick={handleCardClick}
-            />
-          );
-        })}
-      </div>
-      <footer>
-        <div className="score">
-          <div className="moves">
-            <span className="bold">Moves:</span> {moves}
+
+      <div class="float-container">
+        <div class="float-child">
+          <div className="container-cards">
+            {cards.map((card, index) => {
+              return (
+                <Card
+                  key={index}
+                  card={card}
+                  index={index}
+                  isDisabled={shouldDisableAllCards}
+                  isInactive={checkIsInactive(card)}
+                  isFlipped={checkIsFlipped(index)}
+                />
+              );
+            })}
           </div>
-          {localStorage.getItem("bestScore") && (
-            <div className="high-score">
-              <span className="bold">Best Score:</span> {bestScore}
-            </div>
-          )}
         </div>
-        <div className="restart">
-          <Button onClick={handleRestart} color="primary" variant="contained">
-            Restart
-          </Button>
+        <div class="float-child">
+          <div className="container-cards">
+            {cards.map((card, index) => {
+              return (
+                <Card
+                  key={index}
+                  card={card}
+                  index={index}
+                  isDisabled={shouldDisableAllCards}
+                  isInactive={checkIsInactive(card)}
+                  isFlipped={checkIsFlipped(index)}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div class="float-container">
+        <div class="float-child">
+          <Card
+            key={0} //index
+            card={uniqueElementsArray[0]} //card
+            index={0} //index
+            isDisabled={shouldDisableAllCards}
+            isInactive={checkIsInactive(uniqueElementsArray[0])} //card
+            isFlipped={checkIsFlipped(0)} //index
+            onClick={handleCardClick}
+          />
+        </div>
+        <div class="float-child">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="score">
+            <div className="moves">
+              <span className="bold">Moves:</span> {moves}
+            </div>
+            {localStorage.getItem("bestScore") && (
+              <div className="high-score">
+                <span className="bold">Best Score:</span> {bestScore}
+              </div>
+            )}
+          </div>
+          <div className="restart">
+            <Button onClick={handleRestart} color="primary" variant="contained">
+              Restart
+            </Button>
+            <Button onClick={handleAddToBench} color="primary" variant="contained">
+              Add To Bench
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        <p>React + Flask</p>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING PYTHON API</h3>}
         </div>
       </footer>
-
-      <img src={logo} className="App-logo" alt="logo" />
 
       <Dialog
         open={showModal}
@@ -210,44 +299,7 @@ export default function App() {
           </Button>
         </DialogActions>
       </Dialog>
-      <header className="App-header">
-      <p>React + Flask</p>
-      <div>{getMessage.status === 200 ? 
-        <h3>{getMessage.data.message}</h3>
-        :
-        <h3>LOADING PYTHON API</h3>}
-      </div>
-      </header>
+
     </div>
   );
 }
-
-// function App() {
-//   const [getMessage, setGetMessage] = useState({})
-
-//   useEffect(()=>{
-//     axios.get('https://superclub-boardgame-app.herokuapp.com/flask/hello').then(response => {
-//       console.log("SUCCESS", response)
-//       setGetMessage(response)
-//     }).catch(error => {
-//       console.log(error)
-//     })
-
-//   }, [])
-// }
-
-//   return (
-//     <div className="App">
-      // <header className="App-header">
-      //   <img src={logo} className="App-logo" alt="logo" />
-      //   <p>React + Flask Tutorial</p>
-      //   <div>{getMessage.status === 200 ? 
-      //     <h3>{getMessage.data.message}</h3>
-      //     :
-      //     <h3>LOADING</h3>}</div>
-      // </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -1,29 +1,56 @@
 import React from "react";
 import classnames from "classnames";
-import cardback from "./images/stock.jpg";
+import logo from './logo.svg';
 import "./Card.scss";
 
-const Card = ({ onClick, card, index, isInactive, isFlipped, isDisabled }) => {
-  const handleClick = () => {
-    !isFlipped && !isDisabled && onClick(index);
-  };
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      card:   {
+        type: "stock",
+        image: require(`./images/player612.jpeg`)
+      }, 
+      index: -1, 
+      isInactive: false, 
+      isFlipped: false, 
+      isDisabled: false
+    };
 
-  return (
-    <div
-      className={classnames("card", {
-        "is-flipped": isFlipped,
-        "is-inactive": isInactive
-      })}
-      onClick={handleClick}
-    >
-      <div className="card-face card-front-face">
-        <img src={cardback} alt="cardback" />
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('this is:', this);
+    this.setState(prevState => ({
+      isFlipped: !prevState.isFlipped,
+      isDisabled: !prevState.isDisabled,
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    const handleClick = () => {
+      !this.state.isFlipped && !this.state.isDisabled && this.state.onClick(this.state.index);
+    };
+
+    return (
+      <div className={classnames("card", {
+          "is-flipped": this.state.isFlipped,
+          "is-inactive": this.state.isInactive
+        })}
+        onClick={this.handleClick}
+      >
+        <div className="card-face card-front-face">
+          <img src={this.state.card.image} alt="cardback" />
+        </div>
+        <div className="card-face card-back-face">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
       </div>
-      <div className="card-face card-back-face">
-        <img src={card.image} alt="cardback" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Card;
